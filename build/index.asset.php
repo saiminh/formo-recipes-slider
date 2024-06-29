7c013ecbc9f7fcd7f58a827c55fdeecf043ce2e8
saiminh <saiminh@gmail.com>
2024-06-29 10:58:28 +0200

    squooshed image

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'd0921e2db38653d6d579');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'd57fcce931c98e5cc7d1');
